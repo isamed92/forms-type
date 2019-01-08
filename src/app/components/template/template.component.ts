ee3375dc0ad4d7c985d64db8af67947ad19b0bc1
IsaiasPC <isamed92@gmail.com>
2019-01-08 01:38:32 -0300

    form template

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
-  templateUrl: './template.component.html'
+  templateUrl: './template.component.html',
+  styles: []
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: object = {
+    nombre: null,
+    apellido: null,
+    mail: null
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar() {
-    console.log('form posteado');
+  guardar(forma: NgForm) {
+    console.log('ngForm: ' , forma);
+    console.log('value del form: ' , forma.value);
+    console.log('usuario: ' , this.usuario);
 
   }
 
