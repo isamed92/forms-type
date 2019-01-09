75999efddcfb3600bf18c8088052ff0397a3fcb7
IsaiasPC <isamed92@gmail.com>
2019-01-09 17:04:42 -0300

    add asynchronous validators for username

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -37,6 +38,7 @@ export class DataComponent   {
       pasatiempos: new FormArray([
         new FormControl('Correr', [Validators.required])
       ]),
+      username: new FormControl('', [Validators.required], [this.existeUsuario]),
       password1: new FormControl('', [Validators.required]),
       password2: new FormControl()
 
@@ -93,6 +95,23 @@ export class DataComponent   {
       };
     }
     return null;
+  }
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+    let promesa = new Promise(
+      (res, rej) => {
+        setTimeout( () => {
+          if (control.value === 'usuariox') {
+            res( {existe: true} );
+          } else {
+            res( null );
+          }
+        }, 3000);
+      }
+    );
+
+
+    return promesa;
   }
 
 
